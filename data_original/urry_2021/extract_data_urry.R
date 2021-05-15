d5a0c3fc336c92f90a841776be1ee426b3baded4
Henrik Singmann <singmann@gmail.com>
2021-05-15 21:22:22 +0200

    added separate scores to Urry data

--- data_original/urry_2021/extract_data_urry.R
+++ data_original/urry_2021/extract_data_urry.R
@@ -12,12 +12,14 @@ summary(mydata$open_index_prop)
 
 laptop_urry <- mydata %>% 
   #select(ID, condition_label, whichtalklabel, obj_index, open_index) %>% 
-  mutate(performance = (obj_index_prop + open_index_prop)/2 * 100) %>% 
+  mutate(overall = (obj_index_prop + open_index_prop)/2 * 100) %>% 
+  mutate(factual = obj_index_prop * 100,
+         conceptual = open_index_prop * 100) %>% 
   mutate(pid = factor(ID)) %>% 
   mutate(condition = factor(condition_label, 
                             levels = c("laptop", "longhand")),
          talk = factor(whichtalklabel)) %>% 
-  select(pid, condition, talk, performance)
+  select(pid, condition, talk, overall, factual, conceptual)
 
 nrow(laptop_urry)
 
@@ -25,9 +27,9 @@ laptop_urry %>%
   group_by(condition) %>% 
   count()
 
-ggplot(laptop_urry, aes(x = condition, y = performance)) +
+ggplot(laptop_urry, aes(x = condition, y = overall)) +
   geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
   geom_jitter(width = 0.1)
 
-save(laptop_urry, file = "../../../data/urry2021_data.rda")
+save(laptop_urry, file = "../../../data/laptop_urry.rda")
 
