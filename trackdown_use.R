db85afa1e46a606d7f06ce340db5071ae9340429
Henrik Singmann <singmann@gmail.com>
2021-08-24 18:37:08 +0200

    v1 Research Design Chapter

--- trackdown_use.R
+++ trackdown_use.R
@@ -1,15 +1,20 @@
 
 library("trackdown")
 
+"role_of_stats.Rmd"
+"research_designs.Rmd"
+rel_file<- "research_designs.Rmd"
+
 ## step 1: upload file the first time you want to use it:
-upload_file("role_of_stats.Rmd")
+upload_file(rel_file)
 
 ## step 2: make changes on Google doc
 ## THEN ACCEPT ALL CHANGES
 ## then:
-download_file("role_of_stats.Rmd")
-writeLines(iconv(readLines("role_of_stats.Rmd"), from = "", to = "UTF8"), 
-           file("role_of_stats.Rmd", encoding="UTF-8"))
+download_file(rel_file)
+writeLines(iconv(readLines(rel_file), from = "", to = "UTF8"), 
+           file(rel_file, encoding="UTF-8"))
 
 ## then update again on Google docs:
-update_file(file = "role_of_stats.Rmd")
+update_file(file = rel_file)
+
