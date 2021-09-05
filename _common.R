7dd227a02f52c30879c1defa5afa81705fe3a87e
Henrik Singmann <singmann@gmail.com>
2021-09-05 08:54:32 +0200

    switched book to BS4 style

--- _common.R
+++ _common.R
@@ -1,17 +1,23 @@
 set.seed(667)
 
+options(width = 60)
+
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE,
-  out.width = "70%",
-  fig.align = 'center',
-  fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  #cache = TRUE,
+  out.width = "100%",
+  #fig.align = 'center',
+  #fig.width = 4,
+  fig.asp = 0.618 #,  # 1 / phi
+  #fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 
-# Suppress crayon output
-options(crayon.enabled = FALSE)
+# Activate crayon output
+options(
+  crayon.enabled = TRUE,
+  pillar.bold = TRUE,
+  stringr.html = FALSE
+)
