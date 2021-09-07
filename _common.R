46b1fa6bdc8d1b37f8d4d3e0c53d1ad68c439bc9
Henrik Singmann <singmann@gmail.com>
2021-09-07 09:59:32 +0200

    changed default image size in _output.yml

--- _common.R
+++ _common.R
@@ -5,12 +5,12 @@ options(width = 60)
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  #cache = TRUE,
+  cache = TRUE,
   out.width = "100%",
   #fig.align = 'center',
   #fig.width = 4,
-  fig.asp = 0.618 #,  # 1 / phi
-  #fig.show = "hold"
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
