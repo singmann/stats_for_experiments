b37726c7c30f3c8f2e72866bcbf0b04114068793
Henrik Singmann <singmann@gmail.com>
2021-09-10 16:39:54 +0200

    added hidden code to figures

--- R/webex.R
+++ R/webex.R
@@ -1,3 +1,8 @@
 suppressPackageStartupMessages({
   library(webex)
 })
+
+hide_code <- function() {
+  paste0(hide("Show Code"), 
+         "\n\n<div><p><em>Note:</em> The code below is only included for advanced readers or general interest. Understanding the code is not necessary for understanding the content of the chapter.</p></div>\n")
+}
