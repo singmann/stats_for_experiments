272a4d0af7b0e899603e5d18801b2a50bd3d1060
Henrik Singmann <singmann@gmail.com>
2021-09-13 11:36:46 +0200

    with Anas' comments on tidyverse exercises.

--- trackdown_use.R
+++ trackdown_use.R
@@ -6,6 +6,7 @@ rel_file <- "research_designs.Rmd"
 rel_file <- "tidyverse-intro.Rmd"
 rel_file <- "tidyverse-examples-exercises.Rmd"
 rel_file <- "ggplot-intro.Rmd"
+rel_file <- "02-standard_approach.Rmd"
 
 ## step 1: upload file the first time you want to use it:
 upload_file(rel_file)
@@ -20,3 +21,16 @@ writeLines(iconv(readLines(rel_file), from = "", to = "UTF8"),
 ## then update again on Google docs:
 update_file(file = rel_file)
 
+#### Google Doc Links:
+
+# Chapter 1: Role of Stats
+# https://docs.google.com/document/d/1HXOxOW8HZC3dWSCSKa0Rxylpotq7GJF3uthXUIRhHV8/edit?usp=sharing
+
+# Chapter 2: Research Designs 
+# https://docs.google.com/document/d/1HXOxOW8HZC3dWSCSKa0Rxylpotq7GJF3uthXUIRhHV8/edit?usp=sharing
+
+# Chapter 3: Tidyverse Intro
+# https://docs.google.com/document/d/1Eajb4HL9gtecbBJSKjMAk3k-LzLOaNUBQoonoRWNBhQ/edit?usp=sharing
+
+# Chapter 3: Tidyverse Exercises
+# https://docs.google.com/document/d/1ujTfFehV1DCxVNNKJUu9gsFuEUHdV99RWJYi5IIsAqY/edit?usp=sharing
