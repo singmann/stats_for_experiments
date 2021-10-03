02e73c80d189d142374e3e1caf545efc7775f841
Henrik Singmann <singmann@gmail.com>
2021-10-03 13:15:54 +0200

    ggplot exercises

---
+++ extract_r_scripts.R
@@ -0,0 +1,10 @@
+
+rel_file <- "role_of_stats.Rmd"
+rel_file <- "research_designs.Rmd"
+rel_file <- "tidyverse-intro.Rmd"
+rel_file <- "tidyverse-examples-exercises.Rmd"
+rel_file <- "ggplot-intro.Rmd"
+rel_file <- "02-standard_approach.Rmd"
+
+
+knitr::purl(rel_file)
