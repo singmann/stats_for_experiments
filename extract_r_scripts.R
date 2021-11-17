5dc27833a1b1f6cf5b31edc8f4297abffc33b596
Henrik Singmann <singmann@gmail.com>
2021-11-17 19:54:22 +0000

    fixed more links

--- extract_r_scripts.R
+++ extract_r_scripts.R
@@ -6,6 +6,7 @@ rel_file <- "tidyverse-examples-exercises.Rmd"
 rel_file <- "ggplot-intro.Rmd"
 rel_file <- "ggplot-exercises.Rmd"
 rel_file <- "02-standard_approach.Rmd"
+rel_file <- "case_study_mueller_oppenheimer.Rmd"
 
 
 knitr::purl(rel_file)
